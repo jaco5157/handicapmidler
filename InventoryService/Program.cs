e7013889c249ffa970a93dbbf1766170ccbc8e68
Jacob <jacob-dan@hotmail.com>
2022-07-16 18:14:13 +0200

    feat: Create InventoryService template

---
+++ InventoryService/Program.cs
@@ -0,0 +1,10 @@
+using InventoryService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
