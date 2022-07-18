4eebe6bc19ff85b8390ce59a8bf63e9580f80fe4
Jacob <jacob-dan@hotmail.com>
2022-07-18 12:59:11 +0200

    feat: Add ConvertToDTO method

--- InventoryService/DataProviders/CSVDataProvider.cs
+++ InventoryService/DataProviders/CSVDataProvider.cs
@@ -1,11 +1,17 @@
 using InventoryService.Interfaces;
 using InventoryService.DTOs;
+using InventoryService.Models;
 
 namespace InventoryService.DataProviders;
 
 public class CSVDataProvider: IDataProvider
 {
     public IEnumerable<InventoryItemDTO> GetInventoryItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<InventoryItemDTO> ConvertToDTO(IEnumerable<InventoryItemCSV>? items)
     {
         throw new NotImplementedException();
     }
