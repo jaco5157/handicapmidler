554dcb6d6ee30f9498939e8150d11aaa2e1a6a4c
Jacob <jacob-dan@hotmail.com>
2022-07-18 22:08:50 +0200

    feat: Implement CSVDataProvider

--- InventoryService/DataProviders/CSVDataProvider.cs
+++ InventoryService/DataProviders/CSVDataProvider.cs
@@ -1,24 +1,60 @@
+using System.Collections;
 using InventoryService.Interfaces;
 using InventoryService.DTOs;
 using InventoryService.Models;
+using Microsoft.VisualBasic.FileIO;
 
 namespace InventoryService.DataProviders;
 
 public class CSVDataProvider: IDataProvider
 {
+    private string productsFile => Path.Combine("data", "products.json");
+    private string inventoryFile => Path.Combine("data", "products.json");
     public IEnumerable<InventoryItemDTO> GetInventoryItems()
     {
-        throw new NotImplementedException();
+        var items = new Dictionary<string, InventoryItemCSV>();
+        
+        var parser = new TextFieldParser(productsFile);
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters(";");
+
+        while (!parser.EndOfData)
+        {
+            string[] row = parser.ReadFields();
+            if (row != null && row.Length != 0)
+            {
+                items.Add(row[0], new InventoryItemCSV(row[1]));
+            }
+        }
+        
+        parser = new TextFieldParser(inventoryFile);
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters(";");
+        
+        while (!parser.EndOfData)
+        {
+            string[] row = parser.ReadFields();
+            if (row != null && row.Length != 0)
+            {
+                items[row[0]].StockCount = Int32.Parse(row[1]);
+            }
+        }
+        
+        return ConvertToDTO(items);
     }
 
-    public IEnumerable<InventoryItemDTO> ConvertToDTO(IEnumerable<InventoryItemCSV>? items)
+    public IEnumerable<InventoryItemDTO> ConvertToDTO(Dictionary<string, InventoryItemCSV>? items)
     {
         var result = Enumerable.Empty<InventoryItemDTO>();
         if (items != null)
             result = items.Aggregate(result,
-                (current, item) => current.Concat(new[]
-                    {new InventoryItemDTO(item.ProductNumber, item.StockCount, item.DeliveryTime)}));
-
+                (current,
+                    item) => current.Concat(new[]
+                {
+                    new InventoryItemDTO(item.Value.ProductNumber,
+                        item.Value.StockCount,
+                        item.Value.DeliveryTime)
+                }));
         return result;
     }
 }
--- InventoryService/Models/InventoryItemCSV.cs
+++ InventoryService/Models/InventoryItemCSV.cs
@@ -2,9 +2,12 @@ namespace InventoryService.Models;
 
 public class InventoryItemCSV
 {
+    public InventoryItemCSV(string productNumber)
+    {
+        ProductNumber = productNumber;
+    }
+
     public string ProductNumber { get; set; }
-    
-    public int ItideProductNumber { get; set; }
     public int StockCount { get; set; }
-    public int? DeliveryTime { get; set; }
+    public int DeliveryTime { get; set; }
 }
