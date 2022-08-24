e7ae5ca35b60e47752a8c0aa2860cf1d93bb822d
Jacob <jacob-dan@hotmail.com>
2022-08-24 21:54:52 +0200

    feat: Compare inventoryMovement by date

--- InventoryService/Models/InventoryMovement.cs
+++ InventoryService/Models/InventoryMovement.cs
@@ -11,3 +11,14 @@ public class InventoryMovement
     public DateTime MovementDate { get; set; }
     public int Amount { get; set; }
 }
+
+public class DateComparer : IComparer<InventoryMovement>
+{
+    public int Compare(InventoryMovement x, InventoryMovement y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (ReferenceEquals(null, y)) return 1;
+        if (ReferenceEquals(null, x)) return -1;
+        return x.MovementDate.CompareTo(y.MovementDate);
+    }
+}
