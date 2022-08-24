cb669319eb53f06d3dfe99df55114f5f719c1a04
Jacob <jacob-dan@hotmail.com>
2022-08-24 15:47:35 +0200

    feat: Add support for every delivery type

--- deploy/javascripts.js
+++ deploy/javascripts.js
@@ -210,7 +210,7 @@ if(document.querySelector(".webshop-orderstep2")){
 }
 
 function addClickBox() {
-  document.querySelectorAll(".checkout-payment-method, #ShippingMethod_54, #ShippingMethod_55, #GLS_ParselShops_55 tr").forEach(function (item){
+  document.querySelectorAll(".checkout-payment-method, #ShippingMethod_54, #ShippingMethod_55, #ShippingMethod_57, #GLS_ParselShops_55 tr").forEach(function (item){
     item.addEventListener("click", function(event){
       item.querySelector("input").click();
       event.stopPropagation();
