b8261a66014b60f2a78f9f3ec335bef67ae1a05e
Jacob <jacob-dan@hotmail.com>
2023-01-10 15:29:28 +0100

    feat: proper error handing

--- deploy/javascripts.js
+++ deploy/javascripts.js
@@ -258,6 +258,7 @@ if (document.querySelector(".webshop-orderstep1")) {
   trWarning.append(warningBox);
   document.getElementById("Field2_4").insertAdjacentElement("afterend", trWarning);
   document.getElementById("cvrnr").insertAdjacentElement("afterend", cvrButton);
+  tdWarning.className = "LabelColumn";
 
   cvrButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -272,20 +273,21 @@ if (document.querySelector(".webshop-orderstep1")) {
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
           var responseData = JSON.parse(this.response);
-          if (responseData["error"]) return;
+          if (responseData["error"]) throw new Error('Der opstod en fejl, prøv venligst igen.');
           document.getElementById("company").value = responseData["name"];
           document.getElementById("address").value = responseData["address"];
           document.getElementById("zipcode").value = responseData["zipcode"];
           document.getElementById("city").value = responseData["city"];
           document.getElementById("phone").value = responseData["phone"];
           document.getElementById("email").value = responseData["email"];
+          warningBox.style.display = "none";
         }
         if (this.readyState == 4 && this.status != 200) throw new Error('Der opstod en fejl, prøv venligst igen.');
       };
       xhttp.open("GET", fetchurl, true);
       xhttp.send();
     } catch (err) {
-      console.log(err)
+      warningBox.style.display = "block";
     }
   });
 }
